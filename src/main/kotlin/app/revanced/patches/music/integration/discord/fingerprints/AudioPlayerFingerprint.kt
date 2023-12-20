e6584308e3eac39cee68f21265009c66f64b0c03
Vikachubro21 <57052932+Vikachubro21@users.noreply.github.com>
2023-12-19 19:36:53 -0600

    Added Fingerprint for AudioPlayer Class

--- src/main/kotlin/app/revanced/patches/music/integration/discord/fingerprints/AudioPlayerFingerprint.kt
+++ src/main/kotlin/app/revanced/patches/music/integration/discord/fingerprints/AudioPlayerFingerprint.kt
@@ -2,7 +2,7 @@ package app.revanced.patches.music.integration.discord.fingerprints
 
 import app.revanced.patcher.extensions.or
 import app.revanced.patcher.fingerprint.MethodFingerprint
-import app.revanced.patcher.fingerprint.annotation.FuzzyPatternScanMethod
+//import app.revanced.patcher.fingerprint.annotation.FuzzyPatternScanMethod
 import com.android.tools.smali.dexlib2.AccessFlags
 import com.android.tools.smali.dexlib2.Opcode
 
